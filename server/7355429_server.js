3113a3856bffa464e4b0bd3258f09192a41785dc
Coupama Brian <bcbcoupama@gmail.com>
2024-06-01 16:36:09 +0400

    Define Cors request

--- server/7355429_server.js
+++ server/7355429_server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: process.env.CORS_ORIGIN || '*', credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
